8bbe087089db6b35959f026551c27037e5064fde
naresh1-23 <nareshtamang20000@gmail.com>
2023-05-28 20:20:13 +0545

    another changes

--- backend/settings.py
+++ backend/settings.py
@@ -78,8 +78,19 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'onlineland-project',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'Handiplast123@@',
+
+        'HOST': 'localhost',
+
+        'PORT': '',
+
     }
 }
 
